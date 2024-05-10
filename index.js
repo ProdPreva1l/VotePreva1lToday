4ed787b93a46612edd03b76e0b611aab442955f6
ProdPreva1l <prod.preva1l@gmail.com>
2024-05-11 05:14:19 +1000

    Convert to js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const { writeFile } = require("fs/promises");
+
+let currentVotes= 0;
+
+fs.readFile("./data.txt", (err, inputD) => {
+    if (err) throw err;
+    currentVotes = inputD.toString();
+});
+
+app.locals.someFunction = function newVote() {
+    currentVotes++;
+}
+
+app.get(`/api/newVote`, (req, res, next) => {
+    currentVotes++;
+    res.jsonp({status: 'success', currentVotes: currentVotes});
+    writeToFile();
+});
+
+app.use(`/`, (req, res, next) => {
+    res.render('index.pug', { currentVotes: currentVotes });
+});
+
+app.listen(3000, () => {
+    console.log("started!");
+});
+
+async function writeToFile() {
+    await writeFile("./data.txt", currentVotes.toString(), (err) => {
+        if (err) throw err;
+    });
+}

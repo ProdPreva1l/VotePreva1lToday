b9f46077378397b4c475e155a9c45692e0324877
ProdPreva1l <prod.preva1l@gmail.com>
2024-05-11 12:18:19 +1000

    make the disk i/o not happen every time the button gets clicked, added rate limiter

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const fs = require("fs");
 const { writeFile } = require("fs/promises");
+const { rateLimit } = require("express-rate-limit")
+
+const { TaskTimer } = require('tasktimer');
+const timer = new TaskTimer(60000);
 
 let currentVotes= 0;
 
@@ -10,29 +14,46 @@ fs.readFile("./data.txt", (err, inputD) => {
     currentVotes = inputD.toString();
 });
 
-app.locals.someFunction = function newVote() {
-    currentVotes++;
-}
-
-app.get(`/api/newVote`, (req, res, next) => {
-    currentVotes++;
-    res.jsonp({status: 'success', currentVotes: currentVotes});
-    writeToFile();
+const rateLimiter = rateLimit({
+    windowMs: 30 * 60 * 1000,
+    limit: 10,
+    standardHeaders: 'draft-7',
+    legacyHeaders: false,
+    message: {
+        status: 'failure',
+        code: 429,
+        message: "Slow Down Blud"
+    },
+    statusCode: 429
 });
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.get(`/api/newVote`, rateLimiter, (req, res, next) => {
+    currentVotes++;
+    res.jsonp({status: 'success', code: 200});
+    res.end();
+});
+
+app.get(`/api/currentVotes`, (req, res, next) => {
+    res.jsonp({currentVotes: currentVotes});
+    res.end();
+});
 
 app.use(`/`, (req, res, next) => {
     res.render('index.pug', { currentVotes: currentVotes });
 });
 
-
 app.listen(3000, () => {
     console.log("started!");
+    // Start the task to save data to file every minute
+    timer.add(async () => {
+        await writeFile("./data.txt", currentVotes.toString(), (err) => {
+            if (err) throw err;
+        });
+    }).start();
 });
 
-async function writeToFile() {
-    await writeFile("./data.txt", currentVotes.toString(), (err) => {
-        if (err) throw err;
-    });
-}
+app.on("close", async function() {
+    console.log("closing")
+});
